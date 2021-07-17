0b594d4bddbd5b27d8c0e14aac32b3c51dbbeabd
Payal Bhandwalkar <bhandwalkar.payal@gmail.com>
2021-07-17 12:10:29 +0530

    queries+js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,75 @@
 $(document).ready(function() {
-    
-    
-        
+    /* for sticky nav */
+    $('.js--section-features').waypoint(function(direction){
+        if (direction == "down"){
+         $('nav').addClass('sticky');
+        } else {
+         $('nav').removeClass('sticky');
+        }
+    },{
+            offset: '25%'
+          
+    });
+     /*
+     var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+*/   
 
 
+/* scroll on buttons */
+  $('.js--scroll-to-plans').click(function(){
+      $('html,body').animate({scrollTop: $('.js--section-plans').offset().top },1000);
+  });
 
+  $('.js--scroll-to-start').click(function(){
+    $('html,body').animate({scrollTop: $('.js--section-features').offset().top },1000);
+});
 
+/*navigation */
+$(function(){
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+            var target = $(this.hash);
+            target =target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length){
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 
+/**animation */
 
+$('.js--wp-1').waypoint(function(direction){
+    $('.js--wp-1').addClass('animated fadeIn');
+},{
+    offset: '50%'
+});
 
+$('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').addClass('animated fadeInUp');
+},{
+    offset: '50%'
+});
+
+$('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animated fadeIn');
+},{
+    offset: '50%'
+});
+
+$('.js--wp-4').waypoint(function(direction){
+    $('.js--wp-4').addClass('animated pulse');
+},{
+    offset: '50%'
+});
 
 
 
@@ -20,4 +81,4 @@ $(document).ready(function() {
 
 
 }
-)
+);
